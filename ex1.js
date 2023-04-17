ccf38ec444cd3f2b9cc8c238c0ce0fa604edd105
Daniel Cordova <javierdanielcordova@gmail.com>
2023-04-17 09:38:15 +0200

    ejercicio 1 listo

--- ex1.js
+++ ex1.js
@@ -3,9 +3,11 @@
  */
 let vacuna = {
     empresa: 'Pfizer',
-    efectividad: '95'
+    efectividad: '95',
 }
 
 /**
  * Escribe una setencia que muestre por consola la efectividad de la vacuna. SOLO debe mostrar el valor de la propiedad 'efectividad'
  */
+
+console.log(vacuna.efectividad); 
